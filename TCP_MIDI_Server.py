4d52167571f0c46945e16f56c0f28f7f546b297a
ziadm8 <ziad.masri@reseau.eseo.fr>
2024-10-01 12:43:35 +0100

    Update TCP_MIDI_Server.py

--- TCP_MIDI_Server.py
+++ TCP_MIDI_Server.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 10:42:39 2024
+
+@author: ziad2
+"""
 import socket
 import ssl
 import threading
 
 # Load the server's private key and certificate
 server_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-server_context.load_cert_chain(certfile='/home/server_certificate.pem', keyfile='/home/server_private_key.pem')
+server_context.load_cert_chain(certfile="C:/Users/ziad2/Documents/Internship/Projet/TCP_MIDI_SECURE-main/TCP_MIDI_SECURE-main/server_certificate.pem", keyfile="C:/Users/ziad2/Documents/Internship/Projet/TCP_MIDI_SECURE-main/TCP_MIDI_SECURE-main/server_private_key.pem")
 # Create a secure socket and bind to port 8080
 secure_sock = server_context.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_side=True)
 server_address = ('0.0.0.0', 8080)
@@ -18,30 +24,28 @@ clients = {}
 client_id_counter = 0
 client_id_lock = threading.Lock()  # Lock for thread-safe access to the counter
 
-def handle_client(secure_connection, client_address):
-    global client_id_counter
-    global clients
 
-    with client_id_lock:
-        client_id = client_id_counter + 1
-        client_id_counter += 1
+def handle_client(secure_connection, client_address, client_id):
+    global clients
 
     client_ip, client_port = client_address
     print(f"Client connected: ID = {client_id}, IP = {client_ip}, Port = {client_port}")
-
+    public_key = secure_connection.recv(4096)
     # Send the client ID to the connected client
     secure_connection.sendall(str(client_id).encode())
 
     # Store client in the clients dictionary
-    clients[client_id] = {'connection': secure_connection}
+    clients[client_id] = {'connection': secure_connection,
+                          'public_key': public_key
+                          }
+
+    if len(clients) == 2:
+        rsa_key_exchange()
 
     try:
         while True:
             # Receive data from the client
             data = secure_connection.recv(4096)  # Increased buffer size for RTP packets
-            if not data:
-                print(f"Client {client_id} disconnected.")
-                break
 
             # Here you would decrypt the incoming RTP packet before processing it.
             # For now, we will just print it as a placeholder.
@@ -65,12 +69,41 @@ def handle_client(secure_connection, client_address):
         secure_connection.close()
         print(f"Client disconnected: ID = {client_id}, IP = {client_ip}, Port = {client_port}")
 
+
+def rsa_key_exchange():
+    global client1_id, client2_id, client2_connection, client1_connection
+
+    ids = list(clients.keys())
+    print(clients[ids[0]]["public_key"])
+    print(clients[ids[1]]["public_key"])
+    client1_id = ids[0]
+    client2_id = ids[1]
+
+    client1_public_key = clients[client1_id]['public_key']
+    client2_connection = clients[client2_id]['connection']
+
+    print(f"Sending Client {client1_id}'s public key to Client {client2_id}")
+
+    client2_connection.sendall(client1_public_key)
+
+    client2_public_key = clients[client2_id]['public_key']
+    client1_connection = clients[client1_id]['connection']
+
+    print(f"Sending Client {client2_id}'s public key to Client {client1_id}")
+
+    client1_connection.sendall(client2_public_key)
+
+
 while True:
     try:
+
         # Accept a secure connection
         secure_connection, client_address = secure_sock.accept()
+        with client_id_lock:
+            client_id = client_id_counter + 1
+            client_id_counter += 1
         # Create a new thread for each client connection
-        client_thread = threading.Thread(target=handle_client, args=(secure_connection, client_address))
+        client_thread = threading.Thread(target=handle_client, args=(secure_connection, client_address, client_id))
         client_thread.start()
     except Exception as e:
         print(f"Error accepting new connection: {e}")
